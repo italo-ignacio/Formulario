19cac8ef1b1b015036fdc71df0b68307fc49b733
itilocao <italo.felipe.ignacio@hotmail.com>
2022-08-12 17:56:26 -0300

    Testando data para o cel

--- src/components/Pag1/styled.tsx
+++ src/components/Pag1/styled.tsx
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 
 export const StyledInput = styled.input`
+  justify-content: center;
+  text-align: center;
+  align-items: center;
   ::-webkit-datetime-edit-text {
     padding: 0 1rem;
   }
